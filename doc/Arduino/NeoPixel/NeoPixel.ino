ce15cfa3fb6ce306697b180e70eef98c5e969c08
Jesse Schultz <wildstingray@gmail.com>
2019-04-30 12:16:37 -0500

    Update Neopixel file

--- doc/Arduino/NeoPixel/NeoPixel.ino
+++ doc/Arduino/NeoPixel/NeoPixel.ino
@@ -4,8 +4,10 @@
 #include <Adafruit_NeoPixel.h>
 #include <PubSubClient.h>
 
-#define LED_PIN 1
-#define LED_COUNT 10
+#define LED_PIN 26
+#define LED_COUNT 12
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // Update these with values suitable for your network.
 byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
@@ -13,33 +15,25 @@ IPAddress ip(192, 168, 1, 125);
 IPAddress server(192, 168, 1, 121);
 
 //Wifi Stuff
-const char* ssid     = "Greasy Chewbacca";
-const char* password = "123456789xd";
+const char* ssid     = "Jesses10+";
+const char* password = "gobison!";
 WiFiServer wifiCon(80);
 String header;
-String output26State = "off";
-String output27State = "off";
-const int output26 = 26;
-const int output27 = 27;
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i=0;i<length;i++) {
+  for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
 
   if (payload[0] == 0x30)
   {
-    digitalWrite(output26, LOW);
-    digitalWrite(output27, HIGH);
   }
   else
   {
-    digitalWrite(output26, HIGH);
-    digitalWrite(output27, LOW);
   }
 }
 
@@ -54,7 +48,7 @@ void reconnect() {
     if (client.connect("arduinoClient")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("/raspi","Lights Relay Connected");
+      client.publish("/raspi", "Lights Relay Connected");
       // ... and resubscribe
       client.subscribe("/home/lights/1");
     } else {
@@ -71,16 +65,34 @@ void setup()
 {
   Serial.begin(115200);
 
-  pinMode(output26, OUTPUT);
-  pinMode(output27, OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(output26, LOW);
-  digitalWrite(output27, LOW);
+//  pinMode(26, OUTPUT);
+//  pinMode(27, OUTPUT);
+
+  strip.begin();
+  strip.clear();
+
+  int count = 0;
+  bool flip = false;
 
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
+    if (flip)
+    {
+      strip.setPixelColor(11 - count, 0, 0, 0);
+      strip.show();
+      count--;
+      if (count <= -1) flip = !flip;
+    }
+    else
+    {
+      count++;
+      if (count >= 11) flip = !flip;
+      strip.setPixelColor(count, 150, 0, 0);
+      strip.show();
+    }
+    
     delay(500);
     Serial.print(".");
   }
@@ -100,8 +112,22 @@ void setup()
 
 void loop()
 {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+  strip.clear();
+//  if (!client.connected()) {
+//    reconnect();
+//  }
+//  client.loop();
+  
+  
+
+//  strip.clear();
+//  Serial.println("Clearing colors");
+//  for (int i = 0; i < 12; i++) {
+//    strip.setPixelColor(i, count%255, (count*2)%255, (count*3)%255);
+//
+//    strip.show();
+//    count = count + rand()*10;
+//
+//    delay(50);
+//  }
 }
